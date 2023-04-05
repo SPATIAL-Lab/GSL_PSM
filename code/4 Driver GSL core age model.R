4cd57264084e967d5b53a4155ce68eeba90dd639
Deming-Yang <ydmag1987@gmail.com>
2023-04-05 12:36:48 -0600

    added C19 evl

--- code/4 Driver GSL core age model.R
+++ code/4 Driver GSL core age model.R
@@ -18,6 +18,8 @@ max.depth = 1300 #cm
 # Bacon(coredir = mydir, core = "GSL_1B",d.min = 33, d.max = max.depth, d.by=0.5, cc = 1)#age per 0.5 cm
 
 Bacon(coredir = mydir, core = "GSL_1B",d.min = 33, d.max = max.depth, d.by=0.5, cc = 1,run = FALSE)#age per 0.5 cm
+
+agedepth()
 #simple solution: get posterior ages for each depth for all data types
 
 #The posterior ages will be used as JAGS input, then for each data type, sample from the ages
