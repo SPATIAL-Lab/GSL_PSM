1aed4977a0562ebe61e738d0eb6b28fdf6630efe
Deming-Yang <ydmag1987@gmail.com>
2023-05-24 13:50:13 -0600

    update plant wax

--- code/6 Driver GSL core alk dat comp.R
+++ code/6 Driver GSL core alk dat comp.R
@@ -16,7 +16,7 @@ setwd("C:/Users/ydmag/Google Drive/U of U/GSL proxy/GSL_PSM")
 ########## Get organic geochem lipid d2H################
 GSL.FAME.raw <- read.csv("data/GSL_FAME_alt.csv")
 
-d2H.meOH <- -123
+d2H.meOH <- -123.7 #d2H of added methyl group, Huang et al 2002
 #short chains represent lake water
 GSL.FAME.C16 <- GSL.FAME.raw %>% filter(compound=="C16")
 C16 <- 16
@@ -24,10 +24,6 @@ C16 <- 16
 #mass balance correction for FAME values
 GSL.FAME.C16.corr <- GSL.FAME.C16 %>% 
   mutate(d2H.corr = (d2H * (2 * C16 + 2) - 3 * d2H.meOH)/(2 * C16 - 1))
-
-#aggregate data by depth
-GSL.FAME.C16.summ <- GSL.FAME.C16.corr%>% group_by(Depth_cm) %>%
-  summarize(C16.avg = mean(d2H.corr),C16.sd = sd(d2H.corr),PA.16 = mean(area2+area3))
 
 GSL.FAME.C16.naom <- na.omit(GSL.FAME.C16.summ)
 
@@ -39,9 +35,6 @@ C18 <- 18
 GSL.FAME.C18.corr <- GSL.FAME.C18 %>% 
   mutate(d2H.corr = (d2H * (2 * C18 + 2) - 3 * d2H.meOH)/(2 * C18 - 1))
 
-#aggregate data by depth 
-GSL.FAME.C18.summ <- GSL.FAME.C18.corr%>% group_by(Depth_cm) %>%
-  summarize(C18.avg = mean(d2H.corr),C18.sd = sd(d2H.corr),PA.18 = mean(area2+area3))
 
 GSL.FAME.C18.naom <- na.omit(GSL.FAME.C18.summ)
 
@@ -54,8 +47,19 @@ GSL.FAME.C28.corr <- GSL.FAME.C28 %>%
   mutate(d2H.corr = (d2H * (2 * C28 + 2) - 3 * d2H.meOH)/(2 * C28 - 1))
 
 #aggregate data by depth
+GSL.FAME.C16.summ <- GSL.FAME.C16.corr%>% group_by(Depth_cm) %>%
+  summarize(C16.avg = mean(d2H.corr),C16.sd = sd(d2H.corr),PA.16 = mean(area2+area3))
+
+#aggregate data by depth 
+GSL.FAME.C18.summ <- GSL.FAME.C18.corr%>% group_by(Depth_cm) %>%
+  summarize(C18.avg = mean(d2H.corr),C18.sd = sd(d2H.corr),PA.18 = mean(area2+area3))
+
+#aggregate data by depth
 GSL.FAME.C28.summ <- GSL.FAME.C28.corr%>% group_by(Depth_cm) %>%
   summarize(C28.avg = mean(d2H.corr),C28.sd = sd(d2H.corr),PA.28 = mean(area2+area3))
+
+#check sd for each depth
+print(cbind(GSL.FAME.C18.summ$C18.sd,GSL.FAME.C18.summ$Depth_cm))
 
 GSL.FAME.C28.naom <- na.omit(GSL.FAME.C28.summ)
 
